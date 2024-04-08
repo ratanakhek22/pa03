6f0fe6883c3696e571508fb11fa63e860fc51466
tbwagner <twagner2@hawk.iit.edu>
2024-04-08 06:57:59 -0500

    Added test functionality for myv2p()

--- xv6-riscv/user/test_myv2p.c
+++ xv6-riscv/user/test_myv2p.c
@@ -3,9 +3,12 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-    myv2p(0);
-    myv2p(4096);
-    myv2p(12288);
-    myv2p(23482);
+    if (argc != 2) {
+        printf("Usage: %s <virtual address>\n", argv[0]);
+        exit(1);
+    }
+
+    int va = atoi(argv[1]);
+    myv2p(va);
     exit(0);
 }
